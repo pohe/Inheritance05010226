d98f731f4218e08bda31e13c984451a6bb28fba0
Poul Henriksen <pohe@zealand.dk>
2026-01-08 10:27:30 +0100

    8.1 2

---
+++ Inheritance05010226/AnimalRepositoryList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance05010226
+{
+    public class AnimalRepositoryList : IAnimalCollection
+    {
+        private List<Animal> _animals;
+
+        public AnimalRepositoryList()
+        {
+            _animals = new List<Animal>();
+        }
+
+        public int Count { get { return _animals.Count; } }
+
+        public void AddAnimal(Animal animal)
+        {
+            if (animal!= null )
+            {
+                _animals.Add(animal);
+            }
+        }
+
+        public void DeleteAnimal(string name)
+        {
+            //1 udgave
+            //foreach (Animal animal in _animals)
+            //{
+            //    if (animal.Name == name)
+            //    {
+            //        _animals.Remove(animal);
+            //        return; //break
+            //    }
+            //}
+
+            //2 udgave
+            //for(int i =0; i < _animals.Count; i++)
+            //{
+            //    if (_animals[i].Name == name)
+            //    {
+            //        _animals.RemoveAt(i);
+            //        return;
+            //    }
+            //}
+
+            //3. udgave
+            Animal animalToDelete = GetAnimal(name);
+            if (animalToDelete != null)
+            {
+                _animals.Remove(animalToDelete);
+            }
+
+
+
+        }
+
+        public List<Animal> GetAll()
+        {
+            return _animals;
+        }
+
+        public Animal GetAnimal(string name)
+        {
+            foreach(Animal animal in _animals)
+            {
+                if (animal.Name == name)
+                    return animal; 
+            }
+            return null;
+        }
+
+        public void ShowAllAnimals()
+        {
+            //udgave 1
+            //foreach(Animal a in _animals)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //udgave 2
+            //int i = 0;
+            //while (i< _animals.Count)
+            //{
+            //    Console.WriteLine( _animals[i].ToString()  );
+            //    i++;
+            //}
+
+            //Udgave 3
+            for(int i =0; i < _animals.Count; i++)
+            {
+                Console.WriteLine(_animals[i].ToString());
+            }
+
+
+        }
+
+        public bool UpdateAnimal(string name, Animal uddateAnimal)
+        {
+            //Udgave 1
+            Animal animalToUpdate = GetAnimal(name);
+            if (animalToUpdate == null)
+                return false;
+            //animalToUpdate = uddateAnimal; nb virker ikke
+            animalToUpdate.Age = uddateAnimal.Age;
+            animalToUpdate.Name = uddateAnimal.Name;
+            animalToUpdate.Colour = uddateAnimal.Colour;
+            return true;
+        }
+    }
+}
---
+++ Inheritance05010226/IAnimalCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance05010226
+{
+    public interface IAnimalCollection
+    {
+        int Count { get;  }
+
+        List<Animal> GetAll();
+        void AddAnimal(Animal animal);
+        Animal GetAnimal(string name); //Name er unikt
+        void DeleteAnimal(string name);
+
+        bool UpdateAnimal(string name, Animal uddateAnimal);
+
+        void ShowAllAnimals();
+
+    }
+}
